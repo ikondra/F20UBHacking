61cb3f20014ed5b6211e74d3c49c70181243b8e1
Isabelle Ondracek <ikondrac@buffalo.edu>
2020-10-24 15:54:58 -0400

    Changed VertexNode class

--- src/decisionTree/VertexNode.scala
+++ src/decisionTree/VertexNode.scala
@@ -1,14 +1,5 @@
 package decisionTree
 
 class VertexNode(question: String) {
-  var prevEdge: Edge = null
-  var nextEdge: Edge = null
 
-  def isBeginning(): Boolean = {
-    prevEdge == null
-  }
-
-  def isLast(): Boolean = {
-    nextEdge == null
-  }
 }
