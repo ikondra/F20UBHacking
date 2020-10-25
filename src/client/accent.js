535b086def60a6ff377f280a1a6648910f557a9f
MaeveRositzke <55864220+MaeveRositzke@users.noreply.github.com>
2020-10-24 23:47:53 -0400

    Wrote html

--- src/client/accent.js
+++ src/client/accent.js
@@ -2,7 +2,6 @@
 const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
 
-
 //messages received from server
 
 socket.on('nextQuestion', function (question) {
@@ -32,3 +31,27 @@ function restart() {
 function stop() {
     socket.emit("stop");
 }
+
+
+
+document.getElementById("start").onclick = function start() {
+    startDecisions();
+    document.getElementById("content").innerHTML = "<h2 id='nextQuestion'></h2> <button id='yes'>Yes</button> <button id='no'>No</button> <button id='restart'>";
+};
+
+document.getElementById("yes").onclick = function yes() {
+    questionAnswered("yes");
+};
+
+document.getElementById("no").onclick = function no() {
+    questionAnswered("no");
+};
+
+document.getElementById("restart").onclick = function restart() {
+    restart();
+};
+
+document.getElementById("stop").onclick = function stop() {
+    stop();
+    document.getElementById("content").innerHTML = "";
+};
